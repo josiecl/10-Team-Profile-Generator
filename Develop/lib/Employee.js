6e3a9bbaf8307b6d5fb5b955ac81d7d8509f69f2
Josie <josiecollinsrc@gmail.com>
2021-03-10 13:23:45 -0600

    Got the lib files working, along with passing the tests

--- Develop/lib/Employee.js
+++
@@ -1,7 +0,0 @@
-// TODO: Write code to define and export the Employee class
-// Employee has name, id, email, then uses functions getName(), getId(), getEmail(), getRole() (employee)
-class Employee {
-
-}
-
-module.Exports = Employee;
